3ff7a6d16c5a6cba5a5aa73fd314bbc31009558d
John W. Linville <linville@tuxdriver.com>
2011-12-29 12:02:39 -0500

    Implement basic code structure based on Vsync timing.

--- falling.asm
+++ falling.asm
@@ -3,189 +3,52 @@
 
 LOAD	equ	$1c00		Actual load address for binary
 
+SCNBASE	equ	$0400
+SCNSIZE	equ	$0c00
+
+BBLACK	equ	$80
+WBLACK	equ	$8080
+
 	org	LOAD
 
-INIT	clr	$ffc5
+INIT	clr	$ffc5		Select SG12 mode
 
-	ldx	#$0400
-	ldd	#$8080
+	ldx	#SCNBASE	Clear screen
+	ldd	#WBLACK
 
-INILOOP	std	,x++
-	cmpx	#$1000
-	bne	INILOOP
+CLSLOOP	std	,x++
+	cmpx	#(SCNBASE+SCNSIZE)
+	bne	CLSLOOP
 
-*	ldd	#$efef
-*	std	$0a06
-*	std	$0a26
-*	std	$0a18
-*	std	$0a38
-
-	lda	#$8a
-	sta	>COUNT
-
-START	ldx	#$0fcc
-
-	jsr	>CANDY
-
-SYNC	lda	$ff03
-	bpl	SYNC
+* Main game loop is from here to VLOOP
+VSYNC	lda	$ff03		Wait for Vsync
+	bpl	VSYNC
 	lda	$ff02
 
-* start at bottom of screen
-	ldd	#$9faf
-	std	32,x
-	std	34,x
-	std	36,x
-	std	38,x
-	
-	jsr	>CANDY
+	clr	$ffd9		Hi-speed during Vblank
 
-SYNC1	lda	$ff03
-	bpl	SYNC1
-	lda	$ff02
+* Vblank work goes here
 
-	jsr	>CANDY
+	* Paint the side scrolls
 
-* delay for 30fps simulation
-SYNC130	lda	$ff03
-	bpl	SYNC130
-	lda	$ff02
+	* Move the platforms
 
-	jsr	>CANDY
+	* Move the player
 
-* delay for 20fps simulation
-SYNC120	lda	$ff03
-	bpl	SYNC120
-	lda	$ff02
+	* Draw the score
 
-	ldd	#$9faf
-	std	,x
-	std	2,x
-	std	4,x
-	std	6,x
+* Must get here before end of Vblank (~7840 cycles from VSYNC)
+	clr	$ffd8		Lo-speed during display
 
-	ldd	#$af9f
-	std	32,x
-	std	34,x
-	std	36,x
-	std	38,x
+* "Display active" work goes here
 
-LOOP	clr	$ffd9	hi-speed for redraw
-
-	leax	-32,x
-
-	ldd	#$9faf
-	std	,x
-	std	2,x
-	std	4,x
-	std	6,x
-
-	ldd	#$af9f
-	std	32,x
-	std	34,x
-	std	36,x
-	std	38,x
-
-	ldd	#$8080
-
-	std	64,x
-	std	66,x
-	std	68,x
-	std	70,x
-
-	clr	$ffd8	lo-speed for display
-
-	jsr	>CANDY
-
-SYNC2	lda	$ff03
-	bpl	SYNC2
-	lda	$ff02
-
-	jsr	>CANDY
-
-* delay for 30fps simulation
-SYNC230	lda	$ff03
-	bpl	SYNC230
-	lda	$ff02
-
-	jsr	>CANDY
-
-* delay for 20fps simulation
-SYNC220	lda	$ff03
-	bpl	SYNC220
-	lda	$ff02
-
-	cmpx	#$040c
-	bne	LOOP
-
-* erase last two lines at top of screen
-	ldd	#$af9f
-	std	,x
-	std	2,x
-	std	4,x
-	std	6,x
-	ldd	#$8080
-	std	32,x
-	std	34,x
-	std	36,x
-	std	38,x
-
-	jsr	>CANDY
-
-SYNC3	lda	$ff03
-	bpl	SYNC3
-	lda	$ff02
-
-	jsr	>CANDY
-
-* delay for 30fps simulation
-SYNC330	lda	$ff03
-	bpl	SYNC330
-	lda	$ff02
-
-	jsr	>CANDY
-
-* delay for 20fps simulation
-SYNC320	lda	$ff03
-	bpl	SYNC320
-	lda	$ff02
-
-	ldd	#$8080
-	std	,x
-	std	2,x
-	std	4,x
-	std	6,x
-
-CHKUART	lda	$ff69
+* Check for user break (development only)
+CHKUART	lda	$ff69		Check for serial port activity
 	bita	#$08
-	beq	RESTART
+	beq	VLOOP
 	lda	$ff68
-	jmp	[$fffe]
+	jmp	[$fffe]		Re-enter monitor
 
-RESTART	jmp	START
-
-CANDY	ldd	#$0400
-	tfr	d,y
-	lda	>COUNT
-	tfr	a,b
-	adda	#$30
-	ora	#$80
-	eora	#$0f
-	sta	>COUNT
-
-FLOOP	std	,y
-	std	$1e,y
-
-	tfr	a,b
-	adda	#$30
-	ora	#$80
-	eora	#$0f
-	leay	$20,y
-
-	cmpy	#$1000
-	bne	FLOOP
-	rts
-
-COUNT	rmb	1
+VLOOP	jmp	VSYNC
 
 	end	INIT
