476d17d7d3992e46a573eb867aca285e8f19749a
John W. Linville <linville@tuxdriver.com>
2011-12-29 14:18:26 -0500

    Re-implement side-scrolling effect based on new structure, now with three independent scroll-planes.

--- falling.asm
+++ falling.asm
@@ -5,9 +5,13 @@ LOAD	equ	$1c00		Actual load address for binary
 
 SCNBASE	equ	$0400
 SCNSIZE	equ	$0c00
+SCNWIDE	equ	32
+SCNLINS	equ	96
 
 BBLACK	equ	$80
 WBLACK	equ	$8080
+
+SSCLRIN	equ	$b58a		Initial value for side scroll effect
 
 	org	LOAD
 
@@ -20,6 +24,13 @@ CLSLOOP	std	,x++
 	cmpx	#(SCNBASE+SCNSIZE)
 	bne	CLSLOOP
 
+	clr	>FRAMCNT	Initialize frame sequence counter
+
+	ldd	#SSCLRIN	 Seed the side-scroll color data
+	std	>SCRNXTO
+	std	>SCRNXTM
+	std	>SCRNXTI
+
 * Main game loop is from here to VLOOP
 VSYNC	lda	$ff03		Wait for Vsync
 	bpl	VSYNC
@@ -30,6 +41,15 @@ VSYNC	lda	$ff03		Wait for Vsync
 * Vblank work goes here
 
 	* Paint the side scrolls
+	ldx	#$SCNBASE
+SCRLOOP	ldd	SCNWIDE,x
+	std	,x
+	std	(SCNWIDE-2),x
+	leax	SCNWIDE,x
+	cmpx	#(SCNBASE+SCNSIZE-SCNWIDE)
+	bne	SCRLOOP
+	ldd	>SCRNXTO
+	std	,x
 
 	* Move the platforms
 
@@ -42,6 +62,49 @@ VSYNC	lda	$ff03		Wait for Vsync
 
 * "Display active" work goes here
 
+	ldx	#>SCRNXTO	Advance the outer side-scroll color data
+	jsr	>SSCDADV
+
+	lda	>FRAMCNT
+	bne	SKPINR
+
+	* Paint the inner side scrolls
+	ldx	#$SCNBASE
+SCRLPI	ldd	(SCNWIDE+4),x
+	std	4,x
+	std	(SCNWIDE-6),x
+	leax	SCNWIDE,x
+	cmpx	#(SCNBASE+SCNSIZE-SCNWIDE)
+	bne	SCRLPI
+	ldd	>SCRNXTI
+	std	4,x
+
+	ldx	#>SCRNXTI	Advance the inner side-scroll color data
+	jsr	>SSCDADV
+
+	lda	>FRAMCNT
+SKPINR	bita	#$01
+	bne	NXTFRM
+
+	* Paint the middle side scrolls
+	ldx	#$SCNBASE
+SCRLPM	ldd	(SCNWIDE+2),x
+	std	2,x
+	std	(SCNWIDE-4),x
+	leax	SCNWIDE,x
+	cmpx	#(SCNBASE+SCNSIZE-SCNWIDE)
+	bne	SCRLPM
+	ldd	>SCRNXTM
+	std	2,x
+
+	ldx	#>SCRNXTM	Advance the middle side-scroll color data
+	jsr	>SSCDADV
+
+	lda	>FRAMCNT
+NXTFRM	inca
+	anda	#$03
+	sta	>FRAMCNT
+
 * Check for user break (development only)
 CHKUART	lda	$ff69		Check for serial port activity
 	bita	#$08
@@ -50,5 +113,20 @@ CHKUART	lda	$ff69		Check for serial port activity
 	jmp	[$fffe]		Re-enter monitor
 
 VLOOP	jmp	VSYNC
+
+SSCDADV	lda     ,x		Advance the side-scroll color data
+	tfr     a,b		No ldd above, B gets clobbered anyway
+	adda    #$30
+	ora     #$80
+	eora    #$0f
+	std     ,x
+	rts
+
+* Data storage goes here for now, may need a different org later...
+FRAMCNT	rmb	1
+
+SCRNXTO	rmb	2
+SCRNXTM	rmb	2
+SCRNXTI	rmb	2
 
 	end	INIT
